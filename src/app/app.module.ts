c866e9eda2c4ec8da33cfad958fe58dcfcc7246e
nabla160 <albanlabordelaulhe@gmail.com>
2023-10-14 15:22:26 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
